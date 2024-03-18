d2df7bf5370fb5c335e4fe31e50f39e2d7ca6424
lathieuhiep <khacdiepkma90@gmail.com>
2024-03-18 14:47:44 +0700

    update code

--- header.php
+++ header.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php bloginfo('description'); ?>" />
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NBLR6YNT0R"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NBLR6YNT0R');
+    </script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
