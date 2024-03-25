5abf65943167adfbb4c34e11b20bb301656819bd
lathieuhiep <khacdiepkma90@gmail.com>
2024-03-25 12:15:20 +0700

    update code

--- components/inc-chat-with-us.php
+++ components/inc-chat-with-us.php
@@ -10,7 +10,7 @@ $chat_zalo = healthnews_get_option('opt_general_chat_zalo');
 		$zalo_qr_code = $chat_zalo['qr_code'];
 		?>
         <a class="link chat-with-us__zalo" href="https://zalo.me/<?php echo esc_attr( healthnews_preg_replace_ony_number($zalo_phone) ) ?>" data-phone="<?php echo esc_attr($zalo_phone); ?>" data-qr-code="<?php echo esc_attr($zalo_qr_code); ?>">
-            <img alt="zalo" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/bs-trong.jpg' ) ) ?>" width="50" height="" />
+            <img alt="zalo" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/zalo-icon-contact.png' ) ) ?>" width="50" height="" />
         </a>
 	<?php endif; ?>
 
